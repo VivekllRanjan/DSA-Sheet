d685f675465cefd9bb6dcb07e39ea39794f7b2ee
VivekllRanjan <vivek.ranjan1998@gmail.com>
2024-07-27 18:36:55 +0530

    Update

---
+++ Array/SetMatrixZeros.java
@@ -0,0 +1,45 @@
+package DSA;
+
+public class SetMatrixZeros {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 0}, {1, 1, 1}, {1, 1, 1}};
+        int m = matrix.length;
+        System.out.println(m);
+        int n = matrix[0].length;
+        System.out.println(n);
+        int row[] = new int[m];
+        int col[] = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j]);
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < m; i++) {
+            System.out.print(row[i]);
+
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(col[i]);
+
+        }
+        System.out.println();
+        System.out.println("Solution:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Array/SetMatrixZerosOptimal.java
@@ -0,0 +1,46 @@
+package DSA;
+
+public class SetMatrixZerosOptimal {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1}, {1,0,1}, {1,1,1}};
+        int m = matrix.length;
+        System.out.println(m);
+        int n = matrix[0].length;
+        System.out.println(n);
+        int row0 = 0;
+        for(int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j] = 0;
+
+                    if(i > 0){
+                        matrix[i][0] = 0;
+                    }
+                    else{
+                        row0 = 1;
+                    }
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if (matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0){
+            for(int i =0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (row0 == 1){
+            for(int i=0; i<n;i++){
+                matrix[0][i] = 0;
+            }
+        }
+    }
+}
